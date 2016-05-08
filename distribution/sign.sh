62c1d5b628ec1b4e9cd361758d259bb2c48ed532
Gilandel <gilles@landel.fr>
2016-05-08 21:45:47 +0200

    update spring version + add sign gpg key

--- distribution/sign.sh
+++ distribution/sign.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in distribution/signingkey.asc.enc -out distribution/signingkey.asc -d
-    gpg --fast-import distribution/signingkey.asc
+    openssl aes-256-cbc -K $ENCPRYPTED_KEY -iv $ENCPRYPTED_IV -in distribution/signingkey.gpg.enc -out distribution/signingkey.gpg -d
+    gpg --fast-import distribution/signingkey.gpg
 fi
