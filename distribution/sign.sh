42ece9565125bd7799c1c9732bcb19235571a24f
Gilandel <gilles@landel.fr>
2016-05-08 23:04:44 +0200

    change gpg gen

--- distribution/sign.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $ENCPRYPTED_KEY -iv $ENCPRYPTED_IV -in distribution/signingkey.asc.enc -out distribution/signingkey.asc -d
-    gpg --fast-import distribution/signingkey.asc
-fi
