cbd55e9d6cce2a68c9c49d34cfa2d75f36689798
gilles.landel <gilles.landel@open-groupe.com>
2016-05-11 16:17:18 +0200

    correct distribution

--- distribution/prepare.sh
+++ distribution/prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	mkdir -p target/gpg
     openssl aes-256-cbc -K $ENCPRYPTED_KEY -iv $ENCPRYPTED_IV -in distribution/signingkey.asc.enc -out target/gpg/signingkey.asc -d
     gpg --batch --fast-import target/gpg/signingkey.asc
