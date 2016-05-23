40b81756f707003ffeec10f507f0e6e1eed3778c
Gilandel <gilles@landel.fr>
2016-05-23 02:41:27 +0200

    Remove split test

--- utils-model/src/test/java/fr/landel/utils/model/query/QueryBuilderTest.java
+++ utils-model/src/test/java/fr/landel/utils/model/query/QueryBuilderTest.java
@@ -15,17 +15,8 @@ package fr.landel.utils.model.query;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
-
-import fr.landel.utils.model.query.QueryBuilder;
-import fr.landel.utils.model.query.QueryCondition;
-import fr.landel.utils.model.query.QueryOrder;
 
 /**
  * Check query builder.
@@ -55,103 +46,6 @@ public class QueryBuilderTest {
 
         query = new QueryBuilder<>(Zone.class, null);
         assertTrue(query.getAlias().matches("zone\\d+"));
-
-        long diff;
-        long start;
-        String res[];
-
-        System.out.println(Arrays.asList(QueryBuilderTest.split("test;2;dd;;h;", ';', true)));
-        System.out.println(Arrays.asList(QueryBuilderTest.split("test;2;dd;;h;", ';', false)));
-
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < 10_000_000; i++) {
-            list.add(String.format("%d", Math.round(Math.random() * 1000)));
-        }
-
-        for (int j = 0; j < 5; j++) {
-            start = System.currentTimeMillis();
-            for (String test : list) {
-                if (QueryBuilderTest.indexOf(test, '1', 0) > -1) {
-                    res = test.split("1");
-                }
-            }
-            diff = System.currentTimeMillis() - start;
-
-            System.out.println("index gilles: " + diff);
-
-            start = System.currentTimeMillis();
-            for (String test : list) {
-                if (test.indexOf('1') > -1) {
-                    res = test.split("1");
-                }
-            }
-            diff = System.currentTimeMillis() - start;
-
-            System.out.println("index: " + diff);
-
-            start = System.currentTimeMillis();
-            for (String test : list) {
-                res = test.split("1");
-            }
-            diff = System.currentTimeMillis() - start;
-
-            System.out.println("split: " + diff);
-
-            start = System.currentTimeMillis();
-            for (String test : list) {
-                res = QueryBuilderTest.split(test, '1', true);
-            }
-            diff = System.currentTimeMillis() - start;
-
-            System.out.println("split worker: " + diff);
-        }
-    }
-
-    /**
-     * Performs the logic for the {@code split} and
-     * {@code splitPreserveAllTokens} methods that do not return a maximum array
-     * length.
-     *
-     * @param str
-     *            the String to parse, may be {@code null}
-     * @param separatorChar
-     *            the separate character
-     * @return an array of parsed Strings, {@code null} if null String input
-     */
-    private static String[] split(final String str, final char separatorChar, final boolean preserve) {
-        // Performance tuned for 2.0 (JDK1.4)
-
-        if (str == null) {
-            return null;
-        }
-        final int len = str.length();
-        if (len == 0) {
-            return ArrayUtils.EMPTY_STRING_ARRAY;
-        }
-
-        final List<String> list = new ArrayList<>();
-        int off = 0;
-        int next = 0;
-
-        while ((next = QueryBuilderTest.indexOf(str, separatorChar, off)) != -1) {
-            if (preserve || next > off) {
-                list.add(str.substring(off, next));
-            }
-            off = next + 1;
-        }
-        if (preserve || len > off) {
-            list.add(str.substring(off, len));
-        }
-        return list.toArray(new String[list.size()]);
-    }
-
-    private static int indexOf(final String str, final char charSeparator, final int fromIndex) {
-        for (int i = fromIndex; i < str.length(); i++) {
-            if (str.charAt(i) == charSeparator) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     /**
