5862233cf7fe5a7a6c3a625c8b38f69107168839
Gilandel <gilles@landel.fr>
2016-12-19 23:18:53 +0100

    change release conf

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-	echo "Build and deploy SNAPSHOT"
-	mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-	exit $?
-elif [ "${#TRAVIS_TAG}" -gt 0 ]; then
-	echo "Build and deploy RELEASE"
-	git config --global user.email "$GIT_EMAIL"
-	git config --global user.name "$GIT_USER"
-	git fetch upstream
-	git rebase origin/master
-	mvn release:prepare release:perform --batch-mode -DcheckModificationExcludeList=distribution/prepare.sh,distribution/deploy.sh -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-	exit $?
+	if [ "$RELEASE" = "true" ]; then
+		echo "Build and deploy RELEASE"
+		git config --global user.email "$GIT_EMAIL"
+		git config --global user.name "$GIT_USER"
+		mvn release:prepare release:perform --batch-mode -DcheckModificationExcludeList=distribution/prepare.sh,distribution/deploy.sh -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+		exit $?
+	else
+		echo "Build and deploy SNAPSHOT"
+		mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+		exit $?
+	fi
 else
 	echo "Only build"
 fi
