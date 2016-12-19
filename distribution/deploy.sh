6950963e8618e78c57bbb4ebb96c63ab0ed5ad9c
Gilandel <gilles@landel.fr>
2016-12-19 22:30:38 +0100

    change git conf

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -5,10 +5,10 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
 	mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
 	exit $?
 elif [ "${#TRAVIS_TAG}" -gt 0 ]; then
-	export MAVEN_SETTINGS=distribution/settings.xml
 	echo "Build and deploy RELEASE"
-	git config --global user.email "gilles@landel.fr"
-	git config --global user.name "Gilles Landel"
+	git config --global user.email "$GIT_EMAIL"
+	git config --global user.name "$GIT_USER"
+	git rebase master
 	mvn release:prepare release:perform --batch-mode -DcheckModificationExcludeList=distribution/prepare.sh,distribution/deploy.sh -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
 	exit $?
 else
