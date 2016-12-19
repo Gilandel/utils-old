d40858d5ecc7d383f92c926efe2a189e4e77d3af
Gilandel <gilles@landel.fr>
2016-12-19 20:10:43 +0100

    Add release suppot

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-	mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+	if [ "${#TRAVIS_TAG}" -gt 0 ]; then
+		mvn release:prepare release:perform -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+	else
+		mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+	fi
 fi
