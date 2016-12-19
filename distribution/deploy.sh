53fee165ffbcf5e544a7fbd4a807c4af064761dc
Gilandel <gilles@landel.fr>
2016-12-19 23:48:46 +0100

    remove tag version

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-	if [ "$RELEASE" = "true" ]; then
-		echo "Build and deploy RELEASE"
-		git config --global user.email "$GIT_EMAIL"
-		git config --global user.name "$GIT_USER"
-		mvn release:prepare release:perform --batch-mode -DcheckModificationExcludeList=distribution/prepare.sh,distribution/deploy.sh -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-		exit $?
-	else
-		echo "Build and deploy SNAPSHOT"
-		mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-		exit $?
-	fi
+    echo "Build and deploy SNAPSHOT"
+    mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+    exit $?
+elif [ "${#TRAVIS_TAG}" -gt 0 ]; then
+    echo "Build and deploy RELEASE"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global user.name "$GIT_USER"
+    mvn release:clean release:prepare release:perform --batch-mode -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+    exit $?
 else
-	echo "Only build"
+    echo "Only build"
 fi
