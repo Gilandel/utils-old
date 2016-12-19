f30cdda42555e678ef1e1dc20fe0afc6ca315b43
Gilandel <gilles@landel.fr>
2016-12-19 20:37:02 +0100

    Correct deployment

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-	if [ "${#GIT_TAG}" -gt 0 ]; then
-		mvn release:prepare release:perform -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-	else
-		mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-	fi
+	echo "Build and deploy SNAPSHOT"
+	mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+elif [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ]; then
+	echo "Build and deploy RELEASE"
+	mvn release:prepare release:perform -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+else
+	echo "Only build"
 fi
