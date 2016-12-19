6279409b0089501a84e96cba06ad5fa8e83ddf9a
Gilandel <gilles@landel.fr>
2016-12-19 20:27:26 +0100

    Correct

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
-	if [ "${#TRAVIS_TAG}" -gt 0 ]; then
+	if [ "${#GIT_TAG}" -gt 0 ]; then
 		mvn release:prepare release:perform -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
 	else
 		mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
