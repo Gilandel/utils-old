26cb0ff6bd4ecb4eb68e3bbf36ecee8a569670d3
Gilandel <gilles@landel.fr>
2016-12-20 09:18:22 +0100

    use branch

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -4,11 +4,12 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
     echo "Build and deploy SNAPSHOT"
     mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
     exit $?
-elif [ "${#TRAVIS_TAG}" -gt 0 ]; then
+elif [ "$TRAVIS_BRANCH" = 'release' ]; then
     echo "Prepare and perform RELEASE $TRAVIS_TAG"
     git config --global user.email "$GIT_EMAIL"
     git config --global user.name "$GIT_USER"
     mvn release:clean release:prepare release:perform -X -B -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+    git ls-remote .
     exit $?
 else
     echo "Only build"
