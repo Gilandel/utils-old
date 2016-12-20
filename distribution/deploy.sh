74bfa8e5db0a1ea0c2deca928f7f8f4bbed21319
Gilandel <gilles@landel.fr>
2016-12-20 13:21:13 +0100

    correct deployment

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -2,14 +2,22 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
     echo "Build and deploy SNAPSHOT"
+    
     mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-    exit $?
 elif [ "$TRAVIS_BRANCH" = 'release' ]; then
-    echo "Prepare and perform RELEASE $TRAVIS_TAG"
+    echo "Prepare and perform RELEASE"
+    
     git config --global user.email "$GIT_EMAIL"
     git config --global user.name "$GIT_USER"
+    
+    git checkout release
+    
     mvn release:clean release:prepare release:perform -X -B -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-    exit $?
+    
+    git fetch origin +master:master
+    git checkout master
+    git merge release
+    git push origin master
 else
     echo "Only build"
 fi
