74e7fe3d7397382c32a011d3abc4b0cda17767bc
Gilandel <gilles@landel.fr>
2016-12-20 14:58:01 +0100

    add pushing key

--- distribution/prepare.sh
+++ distribution/prepare.sh
@@ -3,9 +3,15 @@
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'release' ]; then
 	    echo "Prepare signing for SNAPSHOT"
+	    
+	    # Decrypt GPG used to sign outputs
 	    mkdir -p target/gpg
 	    openssl aes-256-cbc -K $ENCPRYPTED_KEY -iv $ENCPRYPTED_IV -in distribution/signingkey.asc.enc -out target/gpg/signingkey.asc -d
 	    gpg --batch --fast-import target/gpg/signingkey.asc
+	    
+	    # decrypt SSH key so we can push release to GitHub
+	    openssl aes-256-cbc -K $ENCPRYPTED_KEY -iv $ENCPRYPTED_IV -in distribution/pushingkey.asc.enc -out ${HOME}/.ssh/id_rsa -d
+		chmod 600 ${HOME}/.ssh/id_rsa
 	fi
 else
     echo "No signature required"
