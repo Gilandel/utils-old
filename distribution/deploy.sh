6e104f8156b798ac5b295cee758cefd07d7641a6
Gilandel <gilles@landel.fr>
2016-12-20 08:59:59 +0100

    add release log

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -5,13 +5,10 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" = 'false' ]; then
     mvn deploy -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
     exit $?
 elif [ "${#TRAVIS_TAG}" -gt 0 ]; then
-    echo "Prepare RELEASE $TRAVIS_TAG"
+    echo "Prepare and perform RELEASE $TRAVIS_TAG"
     git config --global user.email "$GIT_EMAIL"
     git config --global user.name "$GIT_USER"
-    mvn release:clean release:prepare --batch-mode -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
-    echo "Perform RELEASE $TRAVIS_TAG"
-    git tag
-    mvn release:perform --batch-mode -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
+    mvn release:clean release:prepare release:perform -X -B -DskipTests=true -P sign,build-extras --settings distribution/settings.xml
     exit $?
 else
     echo "Only build"
